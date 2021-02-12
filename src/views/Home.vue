dce1cc5f74b87a1a6dd311a0064007db07c88f5d
knights1210 <jamis.1984@gmail.com>
2021-02-12 16:55:59 +0900

    section14

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,22 +2,30 @@
   <div>
     <h1>Home</h1>
     <button @click="toUsers">Usersのページに行く</button>
-    <p>{{doubleCount}}</p>
-    <p>{{tripleCount}}</p>
+    <p>{{ doubleCount }}</p>
+    <p>{{ tripleCount }}</p>
+    <input type="text" v-model="message" />
+    <p>{{ message }}</p>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
 export default {
-  computed:
-  {
-    ...mapGetters(["doubleCount","tripleCount"]),
-
+  computed: {
+    ...mapGetters("count",["doubleCount", "tripleCount"]),
+    message: {
+      get() {
+        return this.$store.getters.message;
+      },
+      set(value) {
+        this.$store.dispatch("updateMessage", value);
+      },
+    },
   },
-    // mapGetters({
-    //   myComponentDoubleCount: "doubleCount"
-    // }),
+  // mapGetters({
+  //   myComponentDoubleCount: "doubleCount"
+  // }),
   methods: {
     toUsers() {
       this.$router.push({
